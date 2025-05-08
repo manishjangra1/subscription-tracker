20028f5148f3b6ec2d81133b71d6ebb6df550d43
manishjangra1 <dev.jangramanish@gmail.com>
2025-05-08 14:15:00 +0530

    feat: implement csv export for subscription data

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -7,11 +7,13 @@ import {
   getSubscriptionDetails,
   updateSubscription,
   deleteSubscription,
+  exportSubscriptions,
 } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", authorize, getAllSubscriptions);
+subscriptionRouter.get("/export", authorize, exportSubscriptions);
 subscriptionRouter.get("/:id", authorize, getSubscriptionDetails);
 subscriptionRouter.post("/", authorize, createSubscription);
 subscriptionRouter.put("/:id", authorize, updateSubscription);
